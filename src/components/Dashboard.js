e2c221deba86d2145eec632dd611f4665388bb6a
Gage Thornberry <gagedoug13@gmail.com>
2021-02-24 12:16:50 -0700

    first commit

---
+++ src/components/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Dashboard() {
+    return (
+        <div>
+            <button>ToDo's</button>
+            <button>Goals</button>
+        </div>
+    )
+}
---
+++ src/components/SignIn.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function SignIn() {
+    return (
+        <div>
+            <form>
+                <h3>username</h3>
+                <input />
+
+                <h3>password</h3>
+                <input />
+
+                <button>go</button>
+            </form>
+        </div>
+    )
+}
