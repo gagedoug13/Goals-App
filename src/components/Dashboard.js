6541ba403e8f89c5c19afd23f5497b83d590d366
Gage Thornberry <gagedoug13@gmail.com>
2021-02-26 14:40:30 -0700

    page looks good. working on the to do list next

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,32 +5,33 @@ import { Link } from "react-router-dom"
 export default function Dashboard() {
     return (
         <div className='dashboardDiv'>
+            <div className='dashboardContainerDiv'>
+                <div className='goalsDiv'>
 
-            <div className='goalsDiv'>
-
-                <div className='longGoalsDiv'>
-                    <h2>
+                    <div className='longGoalsDiv'>
+                        <h2>
                         long term goals 
-                    </h2>
-                </div> 
-           
-                <Link to='/goals'>
-                    <button>Goals</button>  
-                </Link> 
-                <div className='shortGoalsDiv'>
-                    <h3>
+                        </h2>
+                    </div> 
+
+                    <Link to='/goals'>
+                        <button>Goals</button>  
+                    </Link> 
+                    
+                    <div className='shortGoalsDiv'>
+                        <h3>
                         short term goals 
-                    </h3>
+                        </h3>
+                    </div>
+
                 </div>
-            
-            </div>
-            
-            <div className='todosDiv'>
-                <Link to='/todos'>
-                    <button>ToDo's</button>
-                </Link>
-            </div>
-           
+
+                <div className='todosDiv'>
+                    <Link to='/todos'>
+                        <button>ToDo's</button>
+                    </Link>
+                </div>
+            </div>   
         </div>
     )
 }
